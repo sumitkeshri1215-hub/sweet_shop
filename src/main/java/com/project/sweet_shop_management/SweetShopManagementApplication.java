f8d939b94432def5d1c0b489fe62661a92d82d10
sumitkeshri1215-hub <sumitkeshri1215@gmail.com>
2025-09-24 22:12:03 +0530

    Initial commit: project structure created

---
+++ src/main/java/com/project/sweet_shop_management/SweetShopManagementApplication.java
@@ -0,0 +1,13 @@
+package com.project.sweet_shop_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SweetShopManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SweetShopManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/sweet_shop_management/SweetShopManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.sweet_shop_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SweetShopManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
